703e628dcac492f0162476cabe1072baeb751d06
JoinQuit <hn58013@gmail.com>
2023-07-02 17:23:44 +0700

    17:23 2/7/2023 ThaisHungw -Fix and Update craw-

--- src/models/schemas/RaceResults.schemas.ts
+++ src/models/schemas/RaceResults.schemas.ts
@@ -8,14 +8,28 @@ export interface RaceResultType {
     ref: 'drivers'
   }
   car: string
-  laps: number
-  timeRetried: string
+  laps: number | string
+  time_retired: string
   pts: number
-  fastest_lap: {
+  fastest_laps: {
     lap: number
     time_of_day: string
     time: string
     avg_speed: number
+  }
+  sprint: {
+    laps: number | string
+    time_retried: string
+    pts: string | number
+  }
+  sprint_grid: {
+    time: string | number
+  }
+  sprint_shootout: {
+    q1: string
+    q2: string
+    q3: string
+    laps: number | string
   }
   pit_stop_summary: [
     {
@@ -25,16 +39,22 @@ export interface RaceResultType {
       total: string
     }
   ]
-  stating_grid: {
+
+  starting_grid: {
     pos: number
     time: string
   }
+
   qualifying: {
-    Q1: string
-    Q2: string
-    Q3: string
+    q1: string
+    q2: string
+    q3: string
     laps: number
   }
+  qualifying_0: any
+  qualifying_1: any
+  qualifying_2: any
+  practice_4: any
   practice_3: {
     time: {
       hour: number
@@ -53,8 +73,13 @@ export interface RaceResultType {
     gap: number
     laps: number
   }
+  practice_0: {
+    time: number
+    gap: number
+    laps: number
+  }
   country: string
-  dateTime: Date
+  date_time: Date
 }
 
 export default class RaceResult {
@@ -65,16 +90,30 @@ export default class RaceResult {
     ref: 'drivers'
   }
   car: string
-  laps: number
-  timeRetried: string
+  laps: number | string
+  time_retired: string
   pts: number
-  fastest_lap: {
+  fastest_laps?: {
     lap: number
     time_of_day: string
     time: string
     avg_speed: number
   }
-  pit_stop_summary: [
+  sprint?: {
+    laps: number | string
+    time_retried: string
+    pts: string | number
+  }
+  sprint_grid?: {
+    time: string | number
+  }
+  sprint_shootout?: {
+    q1: string
+    q2: string
+    q3: string
+    laps: number | string
+  }
+  pit_stop_summary?: [
     {
       lap: number
       time_of_day: string
@@ -82,11 +121,21 @@ export default class RaceResult {
       total: string
     }
   ]
-  stating_grid?: {
+  starting_grid?: {
     pos: number
     time: string
   } | null
-  practice_3: {
+  qualifying?: {
+    q1: string
+    q2: string
+    q3: string
+    laps: number
+  }
+  qualifying_0: any
+  qualifying_1: any
+  qualifying_2: any
+  practice_4?: any
+  practice_3?: {
     time: {
       hour: number
       second: number
@@ -94,33 +143,47 @@ export default class RaceResult {
     gap: number
     laps: number
   }
-  practice_2: {
+  practice_2?: {
     time: number
     gap: number
     laps: number
   }
-  practice_1: {
+  practice_1?: {
+    time: number
+    gap: number
+    laps: number
+  }
+  practice_0?: {
     time: number
     gap: number
     laps: number
   }
   country: string
-  dateTime: Date
+  date_time: Date
   constructor(raceResult: RaceResultType) {
     this._id = raceResult._id
     this.no = raceResult.no
     this.driver = raceResult.driver
     this.laps = raceResult.laps
-    this.timeRetried = raceResult.timeRetried
+    this.time_retired = raceResult.time_retired
     this.pts = raceResult.pts
-    this.fastest_lap = raceResult.fastest_lap
+    this.sprint = raceResult.sprint
+    this.sprint_grid = raceResult.sprint_grid
+    this.sprint_shootout = raceResult.sprint_shootout
+    this.fastest_laps = raceResult.fastest_laps
     this.pit_stop_summary = raceResult.pit_stop_summary
-    this.stating_grid = raceResult.stating_grid
+    this.starting_grid = raceResult.starting_grid
+    this.qualifying = raceResult.qualifying
+    this.qualifying_0 = raceResult.qualifying_0
+    this.qualifying_1 = raceResult.qualifying_1
+    this.qualifying_2 = raceResult.qualifying_2
+    this.practice_4 = raceResult.practice_4
     this.practice_3 = raceResult.practice_3
     this.practice_2 = raceResult.practice_2
     this.practice_1 = raceResult.practice_1
+    this.practice_0 = raceResult.practice_0
     this.car = raceResult.car
     this.country = raceResult.country
-    this.dateTime = raceResult.dateTime
+    this.date_time = raceResult.date_time
   }
 }
